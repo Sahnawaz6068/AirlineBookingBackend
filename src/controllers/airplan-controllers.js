777796df1a2aa7be440c25f4544651fe84e4ef01
Sahnawaz Hussain <sahnawaz643786@gmail.com>
2025-08-02 20:45:42 +0530

    The error of not working airplan is resolve by ->../../generated/prisma

--- src/controllers/airplan-controllers.js
+++ src/controllers/airplan-controllers.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require("../generated/prisma");
+const { PrismaClient } = require("../../generated/prisma");
 const { StatusCodes } = require('http-status-codes');
 
 const client = new PrismaClient();
@@ -6,13 +6,26 @@ const client = new PrismaClient();
 const createAirplane = async (req, res) => {
     const { id, modelNumber, capacity } = req.body;
 
- 
-        // const airplan=await client.a
+ try{
+    const airplane=await client.airplane.create({
+            data:{
+                id:parseInt(capacity),
+                modelNumber,
+                capacity:parseInt(capacity)
+            }
+        })
 
         res.status(StatusCodes.CREATED).json({
             msg: 'Airplane created successfully',
             data: airplane,
         });
+ }
+ catch(err){
+    res.status(StatusCodes.BAD_REQUEST).json({
+        error:err
+    })
+ }
+        
   
 };
 
